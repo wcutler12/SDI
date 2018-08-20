dc683200ff89e80fb7d2334437334af23478fdbd
William Cutler <wcutler12@icloud.com>
2018-08-20 18:15:25 -0400

    Add files via upload

---
+++ NumberVariables/NumberVariables/Program.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Numeric Data Types
+            //Integers
+            //sbyte - signed byte - positive or negative
+            sbyte exSbyte = 50;
+            Console.WriteLine(exSbyte);
+
+            //Short
+            short exShort = 1000;
+            Console.WriteLine(exShort);
+
+            //Integer - int
+            int exInt = 6000000;
+            Console.WriteLine(exInt);
+
+            //Long
+            long exLong = 4000000000;
+            Console.WriteLine(exLong);
+
+            //Floating
+            //Double
+            double exDouble = 67.1234;
+            Console.WriteLine(exDouble);
+
+            //Float
+            float exFloat = 13.3456f;
+            Console.WriteLine(exFloat);
+
+            //Decimal
+            decimal exDecimal = 16.89m;
+            Console.WriteLine(exDecimal);
+
+            //Unsigned - Only positive values
+            byte exByte = 214;
+            Console.WriteLine(exByte);
+
+            //Ushort
+            ushort exUshort = 45023;
+            Console.WriteLine(exUshort);
+
+            //uint
+            uint exUint = 4000000000;
+            Console.WriteLine(exUint);
+
+            //ulong
+            ulong exUlong = 56000000000;
+            Console.WriteLine(exUlong);
+
+        }
+    }
+}
